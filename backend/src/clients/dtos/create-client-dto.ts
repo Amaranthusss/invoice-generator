d2fed6f7007311a31f319938cf97cf401e9de117
Amaranthusss <oskar.szkurlat@gmail.com>
2022-01-02 21:02:11 +0100

    Cleared unused

--- backend/src/clients/dtos/create-client-dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsString } from 'class-validator'
-
-export class CreateClientDto {
-  @IsNumber()
-  id?: number
-
-  @IsString()
-  name: string
-}
--- backend/src/clients/dtos/edit-client-dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsString } from 'class-validator'
-
-export class EditClientDto {
-  @IsNumber()
-  id: number
-
-  @IsString()
-  name: string
-}
