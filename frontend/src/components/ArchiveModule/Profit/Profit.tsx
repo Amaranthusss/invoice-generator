c660466cb63d338eac25c242a3241ad332de451a
Amaranthusss <oskar.szkurlat@gmail.com>
2022-01-25 16:47:54 +0100

    Notify error sending email

--- frontend/src/components/ArchiveModule/Profit/Profit.tsx
+++ frontend/src/components/ArchiveModule/Profit/Profit.tsx
@@ -1,5 +1,9 @@
-import DataSource from 'devextreme/data/data_source'
+import { InitializedEventInfo } from 'devextreme/events'
 import { useRef } from 'react'
+import DataSource from 'devextreme/data/data_source'
+import dxChart from 'devextreme/viz/chart'
+import notify from 'devextreme/ui/notify'
+import _ from 'lodash'
 
 import Chart from '../../_devExtreme/Chart/Chart'
 
@@ -9,10 +13,20 @@ import { IGetTableData } from '../../../../../backend/src/invoices/dtos/getTable
 import { IChartOptions } from '../../_devExtreme/Chart/Chart.interface'
 
 const Profit = (): JSX.Element => {
+  const dxChart = useRef<dxChart>()
+
   const dataSource = useRef<DataSource>(
     new DataSource({
       load: async () => {
-        const store: IGetTableData[] = (await getInvoicesTable()).data
+        dxChart.current?.showLoadingIndicator()
+        const response = await getInvoicesTable()
+        dxChart.current?.hideLoadingIndicator()
+
+        if (!_.inRange(response.status, 200, 299)) {
+          notify('Nie udało się pobrać danych wykresu!', 'error', 5000)
+        }
+
+        const store: IGetTableData[] = response.data
 
         return store
       },
@@ -20,10 +34,15 @@ const Profit = (): JSX.Element => {
     })
   )
 
+  const onInitialized = (e: InitializedEventInfo<dxChart>) => {
+    dxChart.current = e.component
+  }
+
   const chartOptions = useRef<IChartOptions>({
     dataSource: dataSource.current,
     commonSeriesSettings: { argumentField: 'name' },
     series: { type: 'spline', valueField: 'netto' },
+    onInitialized,
   })
 
   return <Chart options={chartOptions.current} />
